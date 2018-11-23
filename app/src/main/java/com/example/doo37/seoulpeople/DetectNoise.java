9d81b5021af5a37f3fe33f20fb5a7f807edc341e
doh01 <37955272+doh01@users.noreply.github.com>
2018-11-23 13:00:14 +0900

    main graph
    dd

---
+++ app/src/main/java/com/example/doo37/seoulpeople/DetectNoise.java
@@ -0,0 +1,54 @@
+package com.example.doo37.seoulpeople;
+
+
+import android.media.MediaRecorder;
+
+import java.io.IOException;
+
+public class DetectNoise {
+    // This file is used to record voice
+    private MediaRecorder mRecorder = null;
+
+    public void start() {
+
+        if (mRecorder == null) {
+
+            mRecorder = new MediaRecorder();
+            mRecorder.setAudioSource(MediaRecorder.AudioSource.VOICE_RECOGNITION);
+            //mRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
+            mRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
+            mRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mRecorder.setOutputFile("/dev/null");
+
+            try {
+                mRecorder.prepare();
+            } catch (IllegalStateException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            mRecorder.start();
+        }
+    }
+
+    public void stop() {
+        if (mRecorder != null) {
+            mRecorder.stop();
+            mRecorder.release();
+            mRecorder = null;
+        }
+    }
+
+    public double getAmplitude() {
+        if (mRecorder != null)
+            //return  (mRecorder.getMaxAmplitude()/2700.0);
+            return   20 * Math.log10(mRecorder.getMaxAmplitude() / 2700.0);
+        else
+            return 0;
+
+    }
+
+}
--- app/src/main/java/com/example/doo37/seoulpeople/MainActivity.java
+++ app/src/main/java/com/example/doo37/seoulpeople/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.doo37.seoulpeople;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Handler;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
@@ -10,9 +13,32 @@ import android.content.pm.PackageManager;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.content.Context;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    // 차트 관련 선언
+    LineChart chart;
+    int X_RANGE = 50;
+    int DATA_RANGE = 40;
+
+    ArrayList<Entry> xVal;
+    LineDataSet setXcomp;
+    ArrayList<String> xVals;
+    ArrayList<ILineDataSet> lineDataSets;
+    LineData lineData;
+
+    private DetectNoise mSensor;
     private Context ct = this;
     private final int MY_PERMISSIONS_RECORD_AUDIO = 1;
 
@@ -23,6 +49,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mSensor = new DetectNoise();
+
+        init();
+        threadStart();
+
         ImageView helpbutton = (ImageView) findViewById(R.id.question);
 
         ImageView startrecord = (ImageView) findViewById(R.id.bt_start);
@@ -32,6 +63,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 onBackPressed();
+
                 Intent intent = new Intent(
                         getApplicationContext(), IntroActivity.class);
                 startActivity(intent);
@@ -43,6 +75,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 onBackPressed();
+                mSensor.stop();
+                handler.removeMessages(0);
                 Intent intent = new Intent(
                         getApplicationContext(), RecordActivity.class);
                 startActivity(intent);
@@ -58,5 +92,113 @@ public class MainActivity extends AppCompatActivity {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},
                     MY_PERMISSIONS_RECORD_AUDIO);
         }
+    }
+
+    private void init(){
+        chart = (LineChart) findViewById(R.id.chart);
+        chartInit();
+    }
+
+
+    private void chartInit() {
+        chart.setAutoScaleMinMaxEnabled(true);
+
+        // chart UI 옵션
+        chart.setBackgroundColor(Color.WHITE);
+        //chart.setDrawGridBackground(false);
+
+        // chart.setDescription("");
+
+        // grid line 비활성화
+        //chart.getXAxis().setDrawGridLines(false);
+        //chart.getAxisLeft().setDrawGridLines(false);
+        //chart.getAxisRight().setDrawGridLines(false);
+
+        chart.getAxisRight().setAxisMinValue(-2.0f);
+        chart.getAxisLeft().setAxisMinValue(-2.0f);
+        chart.getAxisRight().setAxisMaxValue(16.0f);
+        chart.getAxisLeft().setAxisMaxValue(16.0f);
+        chart.getXAxis().setEnabled(false);
+        //chart.getAxisRight().setEnabled(false);
+        //chart.getAxisLeft().setEnabled(false);
+        // chart.getLegend().setEnabled(false);
+
+        xVal = new ArrayList<Entry>();
+        setXcomp = new LineDataSet(xVal, "내 음성");
+
+        // 그래프 선 관련 옵션
+        setXcomp.setColor(Color.BLUE);
+        setXcomp.setDrawValues(false);
+        setXcomp.setDrawCircles(false);
+        setXcomp.setAxisDependency(YAxis.AxisDependency.LEFT);
+        setXcomp.setDrawCubic(true);
+
+        lineDataSets = new ArrayList<ILineDataSet>();
+        lineDataSets.add(setXcomp);
+
+        xVals = new ArrayList<String>();
+        for (int i = 0; i < X_RANGE; i++) {
+            xVals.add("");
+        }
+        lineData = new LineData(xVals,lineDataSets);
+        chart.setData(lineData);
+        chart.invalidate();
+    }
+
+    // 차트 업데이트 함수
+    public void chartUpdate(float x) {
+
+        if (xVal.size() > DATA_RANGE) {
+            xVal.remove(0);
+            for (int i = 0; i < DATA_RANGE; i++) {
+                xVal.get(i).setXIndex(i);
+            }
+        }
+
+        xVal.add(new Entry(x,xVal.size()));
+        setXcomp.notifyDataSetChanged();
+        chart.notifyDataSetChanged();
+        chart.invalidate();
+    }
+
+    // 쓰레드 돌릴 핸들러
+    Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            if (msg.what == 0) {
+                double amp = mSensor.getAmplitude();
+                if (amp < 0)
+                    amp = 0;
+                else if (amp > 12)
+                    amp = Math.sqrt(amp - 12) + 12;
+                float amp2 = (float) amp;
+                chartUpdate(amp2);
+            }
+        }
+    };
+
+
+    // 핸들러 상속 구문
+    class MyThread extends Thread {
+        @Override
+        public void run() {
+
+            while(true) {
+                handler.sendEmptyMessage(0);
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    // 쓰레드 시작 함수
+    private void threadStart() {
+        MyThread thread = new MyThread();
+        thread.setDaemon(true);
+        mSensor.start();
+        thread.start();
     }
 }
