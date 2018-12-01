0c1612e9fee0d936b83c59869f11fad3bb076d8c
ManAboutTown <leinaere@gmail.com>
2018-12-02 06:39:03 +0900

    accumulate data (sentence) now stacked correctly.
    accumulate data (sentence) now stacked correctly.

--- app/src/main/java/com/example/doo37/seoulpeople/ResultActivity.java
+++ app/src/main/java/com/example/doo37/seoulpeople/ResultActivity.java
@@ -29,7 +29,9 @@ import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
 import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.io.File;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class ResultActivity extends AppCompatActivity {
@@ -50,11 +52,10 @@ public class ResultActivity extends AppCompatActivity {
     int BAR_X_RANGE = 50;
     int BAR_DATA_RANGE = 30;
 
-    ArrayList<BarEntry> BAR_xVal;
-    BarDataSet BAR_setXcomp;
-    ArrayList<String> BAR_xVals;
-    ArrayList<IBarDataSet> barDataSets;
-    BarData barData;
+    ArrayList<IBarDataSet> dataSets = null;
+    ArrayList<BarEntry> valueSet = new ArrayList<>();
+    ArrayList<String> xAxis = new ArrayList<>();
+
 
     // 문장 일치율 관련 뷰
     TextView voicediffv;
@@ -75,9 +76,10 @@ public class ResultActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_result);
 
-        // 누적 데이터 관련 SQLite 관리 부분
         sqliteDB = init_database();
         init_tables();
+        // TODO:현재 실행한 테스트를 DB에 저장
+
         load_values();
 
         ImageView Endbutton = (ImageView) findViewById(R.id.Endbutton);
@@ -109,13 +111,13 @@ public class ResultActivity extends AppCompatActivity {
 
         float tempConsistency = (usrsLength / stdsLength) * 100;
         String sentenceConsistency = String.valueOf(tempConsistency);
-        sentenceConsistency = sentenceConsistency +"%";
 
-        Toast.makeText(getApplication(), sentenceConsistency, Toast.LENGTH_LONG).show();
-        sentencediffv.setText(sentenceConsistency);
+        Toast.makeText(getApplication(), sentenceConsistency +"%", Toast.LENGTH_LONG).show();
+        sentencediffv.setText(sentenceConsistency +"%");
 
-        // TODO:현재 실행한 테스트를 DB에 저장
-        save_values();
+        save_values(sentenceConsistency);
+
+        // 누적 데이터 관련 SQLite 관리 부분
     }
 
     private SQLiteDatabase init_database() {
@@ -141,9 +143,10 @@ public class ResultActivity extends AppCompatActivity {
         // 테이블 처음 생성시 초기화
         if (sqliteDB != null) {
             String sqlCreateTbl = "CREATE TABLE IF NOT EXISTS CONTACT_T (" +
-                    "Date " + "INTEGER NOT NULL," +
-                    "voiceC " +
-                    "sentenceC" + ")";
+                    "savedate " + "TEXT NOT NULL," +
+                    "voiceC " + "TEXT NOT NULL," +
+                    "sentenceC " + "TEXT NOT NULL" + ")";
+
             System.out.println(sqlCreateTbl);
             sqliteDB.execSQL(sqlCreateTbl);
         }
@@ -155,37 +158,63 @@ public class ResultActivity extends AppCompatActivity {
             String sqlQueryTbl = "SELECT * FROM CONTACT_T";
             Cursor cursor = null;
 
+            dataSets = null;
+            valueSet = new ArrayList<>();
+            int i = 0;
+            xAxis = new ArrayList<>();
+
             // 쿼리 실행
             cursor = sqliteDB.rawQuery(sqlQueryTbl, null);
-            if (cursor.moveToNext()) {
+            cursor.moveToFirst();
+
+            do {
                 // 레코드가 존재한다면,
 
                 // 값 가져오기.
-                int date = cursor.getInt(0);
-                int voiceC = cursor.getInt(1);
-                int sentenceC = cursor.getInt(2);
+                String date = cursor.getString(0);
+                String voiceC = cursor.getString(1);
+                String sentenceC = cursor.getString(2);
 
-            }
+                xAxis.add(date);
+                BarEntry newdata = new BarEntry(Float.parseFloat(sentenceC), i); // (value, x index)
+                valueSet.add(newdata);
+                i++;
+            } while (cursor.moveToNext());
+
+            cursor.close();
+
+            BarDataSet barDataSet = new BarDataSet(valueSet, "");
+            barDataSet.setColors(ColorTemplate.COLORFUL_COLORS);
+
+            dataSets = new ArrayList<>();
+            dataSets.add(barDataSet);
         }
     }
 
-    private void save_values() {
+    private void save_values(String sentenceConsistency) {
         //TODO: 수행 결과를 데이터베이스에 추가하기.
 
         if (sqliteDB != null) {
 
             // delete
-            sqliteDB.execSQL("DELETE FROM CONTACT_T") ;
+            //sqliteDB.execSQL("DELETE FROM CONTACT_T") ;
 
-            int date = 0;
-            int voiceC = 0;
-            int sentenceC = 0;
+            long now = System.currentTimeMillis();
+            Date date = new Date(now);
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd-HH-mm-ss-SSS");
+            String savedate = sdf.format(date);
+
+            // 임시로 임의 값을 string화 해서 저장
+            String voiceC = "3217";
+
+            String sentenceC = sentenceConsistency;
 
             String sqlInsert = "INSERT INTO CONTACT_T " +
-                    "(date, voiceC, sentenceC) VALUES (" +
-                    Integer.toString(date) + "," +
-                    "'" + Integer.toString(voiceC) + "'," +
-                    "'" + Integer.toString(sentenceC) + "'," + ")" ;
+                    "(savedate, voiceC, sentenceC) VALUES (" +
+                    "'" + savedate + "'," +
+                    "'" + voiceC + "'," +
+                    "'" + sentenceC + "')" ;
 
             System.out.println(sqlInsert) ;
 
@@ -243,6 +272,7 @@ public class ResultActivity extends AppCompatActivity {
         linechart.invalidate();
 
         // Bar 차트 관련 옵션
+        // 현재 화면에 맞게 최대 최소 범위 반응
         //barchart.setAutoScaleMinMaxEnabled(true);
 
         // chart UI 옵션
@@ -268,7 +298,7 @@ public class ResultActivity extends AppCompatActivity {
         // 각 막대별 색상 표시 및 항목 이름 표시
         barchart.getLegend().setEnabled(false);
 
-        BarData bardata = new BarData(getXAxisValues(), getDataSet());
+        BarData bardata = new BarData(xAxis, dataSets);
         bardata.setDrawValues(false);
         barchart.setScaleEnabled(false);
         barchart.setData(bardata);
@@ -278,64 +308,5 @@ public class ResultActivity extends AppCompatActivity {
         barchart.animateXY(1000, 1000);
         barchart.invalidate();
 
-    }
-
-    private ArrayList<IBarDataSet> getDataSet() {
-        //TODO: DB에 저장된 데이터 받아서 밸류로 추가하기
-
-        ArrayList<IBarDataSet> dataSets = null;
-
-        ArrayList<BarEntry> valueSet2 = new ArrayList<>();
-        BarEntry v2e1 = new BarEntry(150.000f, 0); // (value, x index)
-        valueSet2.add(v2e1);
-        BarEntry v2e2 = new BarEntry(90.000f, 1);
-        valueSet2.add(v2e2);
-        BarEntry v2e3 = new BarEntry(120.000f, 2);
-        valueSet2.add(v2e3);
-        BarEntry v2e4 = new BarEntry(60.000f, 3);
-        valueSet2.add(v2e4);
-        BarEntry v2e5 = new BarEntry(20.000f, 4);
-        valueSet2.add(v2e5);
-        BarEntry v2e6 = new BarEntry(80.000f, 5);
-        valueSet2.add(v2e6);
-        BarEntry v2e7 = new BarEntry(150.000f, 6); // (value, x index)
-        valueSet2.add(v2e7);
-        BarEntry v2e8 = new BarEntry(90.000f, 7);
-        valueSet2.add(v2e8);
-        BarEntry v2e9 = new BarEntry(120.000f, 8);
-        valueSet2.add(v2e9);
-        BarEntry v2e10 = new BarEntry(60.000f, 9);
-        valueSet2.add(v2e10);
-        BarEntry v2e11 = new BarEntry(20.000f, 10);
-        valueSet2.add(v2e11);
-        BarEntry v2e12 = new BarEntry(80.000f, 11);
-        valueSet2.add(v2e12);
-
-        BarDataSet barDataSet2 = new BarDataSet(valueSet2, "");
-        barDataSet2.setColors(ColorTemplate.COLORFUL_COLORS);
-
-        dataSets = new ArrayList<>();
-        dataSets.add(barDataSet2);
-
-        return dataSets;
-    }
-
-    private ArrayList<String> getXAxisValues() {
-        //TODO: DB에 저장된 날짜 받아서 X 축으로 추가하기
-
-        ArrayList<String> xAxis = new ArrayList<>();
-        xAxis.add("2018-12-01");
-        xAxis.add("2018-12-02");
-        xAxis.add("2018-12-03");
-        xAxis.add("2018-12-04");
-        xAxis.add("2018-12-05");
-        xAxis.add("2018-12-06");
-        xAxis.add("2018-12-07");
-        xAxis.add("2018-12-08");
-        xAxis.add("2018-12-09");
-        xAxis.add("2018-12-10");
-        xAxis.add("2018-12-11");
-        xAxis.add("2018-12-12");
-        return xAxis;
     }
 }
